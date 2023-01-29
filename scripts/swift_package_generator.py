b0055d1864e8c4b16bb0d7a7945ce0099c45868a
George Prokopenko <green.mobile@mail.ru>
2023-01-29 16:44:46 +0200

    Update swift_package_generator.py

--- scripts/swift_package_generator.py
+++ scripts/swift_package_generator.py
@@ -11,7 +11,6 @@ let package = Package(
     name: "TDLibKit",
     platforms: [
         .watchOS(.v2), // Based on iOS 9 version
-        .tvOS(.v9) // Based on iOS 9 version
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -20,7 +19,7 @@ let package = Package(
             targets: ["TDLibKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/georgeprokopenko/TDLibFramework"),
+        .package(path: "https://github.com/georgeprokopenko/TDLibFramework"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
